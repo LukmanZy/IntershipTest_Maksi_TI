f3b021376ccdb7997a2e7e00e2d17ee86b30df28
Lukman Hakim <hlukman6969@gmail.com>
2022-10-20 21:51:45 +0700

    Program sum of the ten numbers

--- src/main/kotlin/IntershipTest.kt
+++ src/main/kotlin/IntershipTest.kt
@@ -5,10 +5,28 @@ class IntershipTest {
         println()
     }
 
+    fun sumOfTenNumbers() {
+        println("----------Sum of 10 Numbers----------")
+
+        var i = 1
+        var result = 0
+        while (i <= 10) {
+            result += i
+            if (i == 10) {
+                print("$i = ")
+            } else{
+                print("$i + ")
+            }
+            i++
+        }
+        println(result)
+        println()
+    }
 }
 
 fun main() {
     val intershipTest = IntershipTest()
 
     intershipTest.helloWorld()
+    intershipTest.sumOfTenNumbers()
 }
