1a9030239eabcb8b18168f0834128767479d5ba0
Lukman Hakim <hlukman6969@gmail.com>
2022-10-20 21:42:46 +0700

    program Hello World

---
+++ src/main/kotlin/IntershipTest.kt
@@ -0,0 +1,14 @@
+class IntershipTest {
+
+    fun helloWorld() {
+        println("Hello World!")
+        println()
+    }
+
+}
+
+fun main() {
+    val intershipTest = IntershipTest()
+
+    intershipTest.helloWorld()
+}
